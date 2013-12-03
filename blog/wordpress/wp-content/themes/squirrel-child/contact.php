997dc2728cb15b3c6d419aac3d55a17cf2ecca57
abelsoubeyran <abel.soubeyran@gmail.com>
2013-12-03 12:37:06 +0100

    contact

--- blog/wordpress/wp-content/themes/squirrel-child/contact.php
+++ blog/wordpress/wp-content/themes/squirrel-child/contact.php
@@ -21,10 +21,11 @@ Léonore Moncond'huy<br/> <br/>
 
 <b>Pierre MSIKA, en charge du pôle "Organisation interne"</b> <br/>
 <i>pmsika@gmail.com </i><br/>
-Samuel Trouve<br/><br/>
+Samuel Trouve<br/>
+Léonore Moncond'huy<br/><br/>
 
  <b>Irène-Lucie NIORT, en charge du pôle "100 ans d'avenir à Construire" </b><br/>
-<i>congres2014@eeudf.org <i><br/><br/>
+<i>congres2014@eeudf.org </i><br/><br/>
 
 <b>Manon SOUBEYRAN, en charge du pôle "Communication"</b> <br/>
 <i>manon.soub@gmail.com </i><br/>
@@ -34,25 +35,60 @@ Chloé Strack<br/>
 Etienne Pouvreau <br/>
 Vincent Leclerc<br/>
 Roxane De Pol<br/>
+Nicolas Rocher<br/>
+Marie Georget<br/>
 David Fourcaud<br/>
 Abel Soubeyran<br/>
-Samuel Soubeyran
-<br/><br/>
+Samuel Soubeyran<br/><br/>
 
 <b>Pierre BOMPAIRE, en charge du pôle "Gestion administrative et financière"</b><br/>
-<i>pierre.bompaire@gmail.com</i>	<br/><br/>
+<i>pierre.bompaire@gmail.com</i><br/>
+Kim Delagarde<br/>
+Anne-Joelle Filhol<br/>
+Julien Malherbe<br/>
+David St-Girons<br/>
+Adrien Chaboche<br/>
+Delphine Lehot<br/>
+Pierre Forrer<br/>
+Pauline Petit<br/><br/>
 
 <b>Arnaud GBAHOUE, en charge du pôle "Ressources humaines"</b><br/>
-<i>arnaud.gbahoue@gmail.com </i><br/><br/>
+<i>arnaud.gbahoue@gmail.com </i><br/>
+Ludovic Thomas<br/><br/>
 
-<b>Sylvain GRIFFAULT, en charge du pôle "Programmes"</b>	<br/>
-<i>morbleu@wanadoo.fr	</i><br/><br/>
+<b>Sylvain GRIFFAULT, en charge du pôle "Programmes"</b><br/>
+<i>morbleu@wanadoo.fr	</i><br/>
+Vincent De Falguerolles<br/>
+Sébastien Heintz<br/>
+Marine Lucas<br/>
+Martin Jegou<br/>
+Benoit Trouvé<br/>
+Elsa Sautter<br/>
+Claire Gassiat<br/>
+Guillaume Nowak<br/>
+Pierre Gayrault<br/>
+Guillaume Tricot<br/>
+Jean-Gui Vigier<br/>
+Mathieu Geoffriault<br/>
+Kim Delagarde<br/>
+Elsa Bonneau<br/>
+Adrien Chaboche<br/><br/>
 
 <b>Mathieu HARLE, en charge du pôle "Logistique"</b>	<br/>
-<i>mathieu.harle@gmail.com </i><br/><br/>
+<i>mathieu.harle@gmail.com </i><br/>
+Christine Fourcaud<br/>
+Amnie Lanoiselée<br/>
+Claire Denise<br/>
+Jérôme Kasparian<br/>
+Mathieu Harlé<br/>
+Sophie Chevalier<br/>
+Julien Chraibi<br/>
+Véronique Sureau<br/><br/>
 
 <b>Elodie ROL, en charge du pôle  "Vie de camp"</b> <br/>
-<i>rol.elodie@gmail.com </i><br/><br/>
+<i>rol.elodie@gmail.com </i><br/>
+Pierre Bodroux<br/>
+Florent Volpelière<br/><br/>
 
         </div>
     </div>
